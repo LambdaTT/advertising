61139018a43e5b7de6c3fc84ca26d3f1f346c74a
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-09-12 18:30:40 -0300

    added new media channel seed

--- dbseeds/1757442494_add-email-media-channel.php
+++ dbseeds/1757442494_add-email-media-channel.php
@@ -4,7 +4,7 @@ namespace Advertising\Seeds;
 
 use SplitPHP\DbManager\Seed;
 
-class AddDefaultMediaChannels extends Seed
+class AddEmailMediaChannel extends Seed
 {
   public function apply()
   {
@@ -15,13 +15,5 @@ class AddDefaultMediaChannels extends Seed
       ->onField('ds_title')->setFixedValue('E-mail')
       ->onField('ds_service_uri')->setFixedValue('advertising/media')
       ->onField('ds_service_method')->setFixedValue('sendByEmail');
-
-    $this->SeedTable('ADV_MEDIACHANNEL', batchSize: 1)
-      ->onField('ds_key', true)->setByFunction(function () {
-        return 'med-' . uniqid();
-      })
-      ->onField('ds_title')->setFixedValue('App Notification')
-      ->onField('ds_service_uri')->setFixedValue('advertising/media')
-      ->onField('ds_service_method')->setFixedValue('sendAppNotification');
   }
 }
---
+++ dbseeds/1757712573_add-appnotification-media-channel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Advertising\Seeds;
+
+use SplitPHP\DbManager\Seed;
+
+class AddAppnotificationMediaChannel extends Seed
+{
+  public function apply()
+  {
+    $this->SeedTable('ADV_MEDIACHANNEL', batchSize: 1)
+      ->onField('ds_key', true)->setByFunction(function () {
+        return 'med-' . uniqid();
+      })
+      ->onField('ds_title')->setFixedValue('App Notification')
+      ->onField('ds_service_uri')->setFixedValue('advertising/media')
+      ->onField('ds_service_method')->setFixedValue('sendAppNotification');
+  }
+}
